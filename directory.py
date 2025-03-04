0ec514f62db2fd2620494b3e9cdc05592bd22296
poulse <anonimoy29@gmail.com>
2025-03-03 21:29:04 -0400

    pruebas

---
+++ directory.py
@@ -0,0 +1,25 @@
+import shutil ,os
+
+def directory():
+    directorios = [
+        os.path.expanduser("~/repos"),
+        os.path.expanduser("~/.config/bspwm/scripts"),
+        os.path.expanduser("~/.config/sxhkd"),
+        os.path.expanduser("~/.config/polybar"),
+        os.path.expanduser("/usr/local/share/fonts"),
+    ]
+    for directorio in directorios:
+        try:
+            os.makedirs(directorio, exist_ok=True)
+            print("")
+            print(f"Directorio {directorio}  listo")
+        except Exception as e:
+            print(f"Error creando {directorio} : {str(e)}")
+"""    try:
+        shutil.copy(
+            os.path.join(os.getcwd(), "sxhkdrc"),
+            os.path.expanduser("~/.config/sxhkd/sxhkdrc")
+        )
+    except Exception as e:
+        print(f"Error copiando sxhkdrc: {str(e)}")
+"""
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os ,sys, shutil
 import time
-
+import directory
 
 def is_root():
     return os.geteuid() == 0
@@ -45,35 +45,39 @@ def menu():
         pass
     if option == "5":
         exit()
- 
+
 def req():
     print("[+] Instalando requerimientos...\n")
     os.system("sudo pacman  -S --noconfirm sudo pacman -S libconfig-devel dbus-devel libev-devel libepoxy-devel pcre2-devel pixman-devel xorgproto libx11-devel libxcb-devel libxcb-composite-devel libxcb-damage-devel libxcb-glx-devel libxcb-image-devel libxcb-present-devel libxcb-randr-devel libxcb-render-devel libxcb-render-util-devel libxcb-shape-devel libxcb-xfixes-devel xcb-util-devel mesa-devel meson ninja uthash")
     os.system("sudo pacman  -S --noconfirm base-devel git vim xcb-util xcb-util-wm xcb-util-keysyms xcb-util-xrm libxcb xorg-xrandr alsa-lib libxinerama")
     os.system("sudo pacman -S --noconfirm bspwm kitty polybar rofi meson cmake libev uthash libepoxy pkgconf xorg-server xorg-xinit glfw-x11" )
-    os.system("sudo pacman -S --noconfirm 7zip")
+    os.system("sudo pacman -S --noconfirm 7zip zsh neofetch")
     time.sleep(2)
     print("[+] Requetimientos instalados correctamente")
 
 def bspwm():
-    os.system("mkdir ~/repos && cd ~/repos && git clone https://github.com/baskerville/bspwm.git")
+    print("[+] Creando directorios.....")
+    directory()
+    time.sleep(2)
+    os.system("cd ~/repos && git clone https://github.com/baskerville/bspwm.git")
     os.system("cd ~/repos/bspwm && make && sudo make install")
     os.system("cd ~/repos && git clone https://github.com/baskerville/sxhkd.git")
     os.system("cd ~/repos/sxhkd && make && sudo make install")
 
     os.system("cd ~/repos && git clone https://github.com/yshui/picom")
     os.system("cd ~/repos/picom  && meson setup --buildtype=release build  && ninja -C build")
-    #ninja -C build install
+    os.system("ninja -C build install")
 
 def conf():
-    os.system("mkdir -p ~/.config/bspwm/scripts && mkdir ~/.config/sxhkd")
+    #os.system("mkdir -p ~/.config/bspwm/scripts && mkdir ~/.config/sxhkd")
     shutil.copy(os.path.join(os.getcwd(),"sxhkdrc"), os.path.expanduser("~/.config/sxhkd/"))
     shutil.copy(os.path.join(os.getcwd(),"bspwm_resize"), os.path.expanduser("~/.config/bspwm/scripts"))
+    shutil.copy(os.path.join(os.getcwd(),"bspwmrc"),os.path.expanduser("~/.config/bspwm/"))
     os.system("chmod +x ~/.config/bspwm/scripts/bspwm_resize")
 
 def polybar():
     #os.system("mkdir -p /usr/local/share/fonts")
-    shutil.copy(os.path.join(os.getcwd(),"recursos/Hack.zip"),os.path.expanduser("/usr/local/share/fonts") )
-    os.system("7z x /usr/local/fonts/Hack.zip")
+    shutil.copy(os.path.join(os.getcwd(),"recursos/Hack.zip"),os.path.expanduser("/usr/local/share/fonts/") )
+    os.system("7z x /usr/local/share/fonts/Hack.zip")
 
 menu()
